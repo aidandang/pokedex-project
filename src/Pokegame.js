d244bb4b8666db6f6fa9da28386c2bff817d82cd
Aidan Dang <aidandang@gmail.com>
2020-03-27 08:59:30 -0700

    added Pokegame fully functioning component with CSS

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Pokedex from './Pokedex';
+import './Pokegame.css';
 
 class Pokegame extends Component {
   static defaultProps = {
@@ -15,8 +17,26 @@ class Pokegame extends Component {
   }
 
   render() {
+    let hand1 = [];
+    let hand2 = [...this.props.pokemon];
+
+    while (hand1.length < hand2.length) {
+      let randIdx = Math.floor(Math.random() * hand2.length);
+      let randPokemon = hand2.splice(randIdx, 1)[0];
+      hand1.push(randPokemon);
+    }
+
+    let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+    let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+
     return (
-      <div></div>
+      <div className="Pokegame">
+        <h1>Pokegame</h1>
+        <Pokedex pokemon={hand1} exp={exp1} isWinner={exp1 > exp2} />
+        <Pokedex pokemon={hand2} exp={exp2} isWinner={exp2 > exp1} />
+      </div>
     )
   }
 }
+
+export default Pokegame;
