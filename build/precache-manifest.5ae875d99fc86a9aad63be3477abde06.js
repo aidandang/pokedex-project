d7877cc3b0c4d5a42524def9fc431273f17cda5b
Aidan Dang <aidandang@gmail.com>
2020-03-26 21:39:51 -0700

    added files

---
+++ build/precache-manifest.5ae875d99fc86a9aad63be3477abde06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ced9522900551d672dc665cda094c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "035d887d9e4f2fa4a15e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d60e958b479cdec6588e",
+    "url": "/static/js/2.80cae23c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.80cae23c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035d887d9e4f2fa4a15e",
+    "url": "/static/js/main.4c5b1fc7.chunk.js"
+  },
+  {
+    "revision": "fb01dcf1aecacc333092",
+    "url": "/static/js/runtime-main.8f90365b.js"
+  }
+]);
